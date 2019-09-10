e1ac358493fe634f9e58fac1520eda626709aca9
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-10 19:45:24 -0400

    added all questions with corresponding answers.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,73 +21,73 @@ $(document).ready(function () {
 
         {
 
-            question: '',
-            choices: ['a', 'b', 'c', 'd'],
-            answer: 'd'
+            question: 'Every episode of Seinfeld contains an image or reference to what superhero ?',
+            choices: ['Plastic Man', 'Superman ', 'Spiderman', 'Batman'],
+            answer: 'Superman'
 
         },
 
         {
 
-            question: '',
-            choices: ['a', 'b', 'c', 'd'],
-            answer: 'a'
+            question: 'Which pop star played 27 different instruments on their debut album For You?',
+            choices: ['Michael Jackson', 'Guns n Roses', 'Prince', 'Boyz 2 Men'],
+            answer: 'Prince'
 
         },
     
         {
 
-            question: '',
-            choices: ['a', 'b', 'c', 'd'],
-            answer: 'a'
+            question: "What was the name of Norm's wife on Cheers?",
+            choices: ['Vera', 'Denise', 'Lucy', 'Sally'],
+            answer: 'Vera'
 
         },
 
         {
 
-            question: '',
-            choices: ['a', 'b', 'c', 'd'],
-            answer: 'a'
+            question: 'What was the first sport televised in the U.S.?',
+            choices: ['Ping Pong', 'Horse Racing', 'Baseball', 'Football'],
+            answer: 'Baseball'
 
         },
 
         {
 
-            question: '',
-            choices: ['a', 'b', 'c', 'd'],
-            answer: 'a'
+            question: 'What was the first movie by Pixar to receive a rating higher than G in the United States?',
+            choices: ['Finding Nemo', 'The Incredibles', 'Toy Story 4', 'Monsters Inc'],
+            answer: 'The Incredibles'
 
         },
 
         {
 
-            question: '',
-            choices: ['a', 'b', 'c', 'd'],
-            answer: 'a'
+            question: 'What color are aircraft black boxes? ',
+            choices: ['Bright orange ', 'Black', 'Pink ', 'Transparent '],
+            answer: 'Bright orange '
 
         },
 
         {
 
-            question: '',
-            choices: ['a', 'b', 'c', 'd'],
-            answer: 'a'
+            question: 'What famous general was once attacked by rabbits ?',
+            choices: ['Alexander The Great', 'Julius Caesar', 'Genghis Khan', 'Napolean '],
+            answer: 'Napolean '
 
         },
 
         {
 
-            question: '',
-            choices: ['a', 'b', 'c', 'd'],
-            answer: 'a'
+            question: 'Which NFL team appeared in four consecutive Super Bowls from 1991 - 1994 and lost them all? ',
+            choices: ['Buffalo Bills ', 'Washington Redskins', 'Oakland Raiders', 'Chicago Cubs'],
+            answer: 'Buffalo Bills '
 
         },
 
         {
 
-            question: '',
-            choices: ['a', 'b', 'c', 'd'],
-            answer: 'a'
+            question: 'What is the name of the hobbit played by Elijah Wood in the Lord of the Rings movies?',
+            choices: ['Luke Skywalker ', 'Aragorn ', 'Boromir', 'Frodo '],
+            answer: 'Frodo '
 
         }
     
