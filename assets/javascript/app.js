18eb337dfe8e2ecde1f92bb42b5be507df32826d
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-10 17:58:39 -0400

    added some global variables.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,40 @@
 $(document).ready(function () {
 
-var triviaQuestions = [ 
-    {
+    //global variables//
+    var questionNumber = 0;
+    var timer;
+    var counter = 30;
+    var correct = 0;
+    var incorrect = 0;
 
-        question: '',
-        choices: ['a','b','c','d'],
-        answer: 'c'
-    
-     },
 
-     {
 
-        question: '',
-        choices: ['a','b','c','d'],
-        answer: 'c'
-    
-     },
+    //variable with objects for references with userchoices//
+    var triviaQuestions = [
+        {
 
-     {
+            question: '',
+            choices: ['a', 'b', 'c', 'd'],
+            answer: 'c'
 
-        question: '',
-        choices: ['a','b','c','d'],
-        answer: 'c'
-    
-     }
- ];
+        },
+
+        {
+
+            question: '',
+            choices: ['a', 'b', 'c', 'd'],
+            answer: 'd'
+
+        },
+
+        {
+
+            question: '',
+            choices: ['a', 'b', 'c', 'd'],
+            answer: 'a'
+
+        }
+    ];
 
 
 
