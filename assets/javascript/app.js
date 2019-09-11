06b088b7e9f51d3b91266f23783f140ddf8ab929
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-11 17:51:12 -0400

    added further conditionals to on click function. More css styling done.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
     //global variables//
     var questionNumber = 0;
     var timer;
-    var counter = 5;
+    var counter = 30;
     var correct = 0;
     var incorrect = 0;
 
@@ -12,80 +12,80 @@
         {
 
             question: 'In the popular TV show The Office, which restaurant chain was Pam banned from? ',
-            choices: ['&nbsp a: &nbsp; Taco Bell &nbsp', '&nbsp b: &nbsp; Carrabbas &nbsp', '&nbsp c: &nbsp; A Steakhouse &nbsp', "&nbsp d: &nbsp; Chili's &nbsp"],
-            answer: "&nbsp d: &nbsp; Chili's &nbsp"
+            choices: [' a: Taco Bell ', ' b: Carrabbas ', ' c: A Steakhouse '," d: Chili's "],
+            answer: " d: Chili's "
 
         },
 
         {
 
             question: 'Every episode of Seinfeld contains an image or reference to what superhero ?',
-            choices: ['&nbsp a: &nbsp; Plastic Man &nbsp', '&nbsp b: &nbsp; Superman &nbsp', '&nbsp c: &nbsp; Spiderman &nbsp', '&nbsp d: &nbsp; Batman &nbsp'],
-            answer: '&nbsp b: &nbsp; Superman &nbsp'
+            choices: [' a: Plastic Man ', ' b: Superman ', ' c: Spiderman ', ' d: Batman '],
+            answer: ' b: Superman '
 
         },
 
         {
 
             question: 'Which pop star played 27 different instruments on their debut album For You?',
-            choices: ['&nbsp a: &nbsp; Michael Jackson &nbsp', '&nbsp b: &nbsp; Guns n Roses &nbsp', '&nbsp c: &nbsp; Prince &nbsp', '&nbsp d: &nbsp; Boyz 2 Men &nbsp'],
-            answer: '&nbsp c: &nbsp; Prince &nbsp'
+            choices: [' a: Michael Jackson ', ' b: Guns n Roses ', ' c: Prince ', ' d: Boyz 2 Men '],
+            answer: ' c: Prince '
 
         },
 
         {
 
             question: "What was the name of Norm's wife on Cheers?",
-            choices: ['&nbsp a: &nbsp; Vera &nbsp', '&nbsp b: &nbsp; Denise &nbsp', '&nbsp c: &nbsp; Lucy &nbsp', '&nbsp d: &nbsp; Sally &nbsp'],
-            answer: '&nbsp a: &nbsp; Vera &nbsp'
+            choices: [' a: Vera ', ' b: Denise ', ' c: Lucy ', ' d: Sally '],
+            answer: ' a: Vera '
 
         },
 
         {
 
             question: 'What was the first sport televised in the U.S.?',
-            choices: ['&nbsp a: &nbsp; Ping Pong &nbsp', '&nbsp b: &nbsp; Horse Racing &nbsp', '&nbsp c: &nbsp; Baseball &nbsp', '&nbsp d: &nbsp; Football &nbsp'],
-            answer: '&nbsp c: &nbsp; Baseball &nbsp'
+            choices: [' a: Ping Pong ', ' b: Horse Racing ', ' c: Baseball ', ' d: Football '],
+            answer: ' c: Baseball '
 
         },
 
         {
 
             question: 'What was the first movie by Pixar to receive a rating higher than G in the United States?',
-            choices: ['&nbsp a: &nbsp; Finding Nemo &nbsp', '&nbsp b: &nbsp; The Incredibles &nbsp', '&nbsp c: &nbsp; Toy Story 4 &nbsp', '&nbsp d: &nbsp; Monsters Inc &nbsp'],
-            answer: '&nbsp b: &nbsp; The Incredibles &nbsp'
+            choices: [' a: Finding Nemo ', ' b: The Incredibles ', ' c: Toy Story 4 ', ' d: Monsters Inc '],
+            answer: ' b: The Incredibles '
 
         },
 
         {
 
             question: 'What color are aircraft black boxes? ',
-            choices: ['&nbsp a: &nbsp; Bright orange &nbsp', '&nbsp b: &nbsp; Black &nbsp', '&nbsp c: &nbsp; Pink &nbsp', '&nbsp d: &nbsp; Transparent &nbsp'],
-            answer: '&nbsp a: &nbsp; Bright orange &nbsp'
+            choices: [' a: Bright orange ', ' b: Black ', ' c: Pink ', ' d: Transparent '],
+            answer: ' a: Bright orange '
 
         },
 
         {
 
             question: 'What famous general was once attacked by rabbits ?',
-            choices: ['&nbsp a: &nbsp; Alexander The Great &nbsp', '&nbsp b: &nbsp; Julius Caesar &nbsp', '&nbsp c: &nbsp; Genghis Khan &nbsp', '&nbsp d: &nbsp; Napolean &nbsp'],
-            answer: '&nbsp d: &nbsp; Napolean &nbsp'
+            choices: [' a: Alexander The Great ', ' b: Julius Caesar ', ' c: Genghis Khan ', ' d: Napolean '],
+            answer: ' d: Napolean '
 
         },
 
         {
 
             question: 'Which NFL team appeared in four consecutive Super Bowls from 1991 - 1994 and lost them all? ',
-            choices: ['&nbsp a: &nbsp; Buffalo Bills &nbsp', '&nbsp b: &nbsp; Washington Redskins &nbsp', '&nbsp c: &nbsp; Oakland Raiders &nbsp', '&nbsp d: &nbsp; Chicago Cubs &nbsp'],
-            answer: '&nbsp a: &nbsp; Buffalo Bills &nbsp'
+            choices: [' a: Buffalo Bills ', ' b: NY Jets ', ' c: Oakland Raiders ', ' d: Chicago Cubs '],
+            answer: ' a: Buffalo Bills '
 
         },
 
         {
 
             question: 'What is the name of the hobbit played by Elijah Wood in the Lord of the Rings movies?',
-            choices: ['&nbsp a: &nbsp; Luke Skywalker &nbsp', '&nbsp b: &nbsp; Aragorn &nbsp', '&nbsp c: &nbsp; Boromir &nbsp', '&nbsp d: &nbsp; Frodo &nbsp'],
-            answer: '&nbsp d: &nbsp; Frodo &nbsp'
+            choices: [' a: Luke Skywalker ', ' b: Aragorn ', ' c: Boromir ', ' d: Frodo '],
+            answer: ' d: Frodo '
 
         }
 
@@ -100,7 +100,7 @@
     //function to load questions and choices//
     function questionGenerator() {
 
-        counter = 5;
+        counter = 30;
 
         var newQuestion = triviaQuestions[questionNumber].question;
         var choices = triviaQuestions[questionNumber].choices;
@@ -177,6 +177,29 @@
         }
     }
 
+    //enable on click function and obtain userAnswer values and apply conditionals//
+    $(document).on('click', '.choice', function() {
+    
+            var userAnswer = $(this).attr('data-answer');
+            var answer = triviaQuestions[questionNumber].answer;
 
+            if (userAnswer == answer) {
+
+                correct++;
+                $("#correct").html('&nbsp Correct: ' + correct);
+                clearInterval(timer);
+                nextQuestion();
+
+            }
+
+            else {
+                incorrect++;
+                $("#incorrect").html('&nbsp Incorrect: ' + incorrect);
+                clearInterval(timer);
+                nextQuestion();
+            }
+
+            console.log('testing', userAnswer, answer);
+    });
 
  
