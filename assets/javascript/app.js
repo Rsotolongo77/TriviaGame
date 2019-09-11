dc665fd5230a9438024fd2fb6587fe400286e97b
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-11 06:00:07 -0400

    added css styling to choices.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,80 +14,80 @@ $(document).ready(function () {
         {
 
             question: 'In the popular TV show The Office, which restaurant chain was Pam banned from? ',
-            choices: ['Taco Bell', 'Carrabbas', 'A Steakhouse', "Chili's"],
-            answer: "Chili's"
+            choices: ['&nbsp a: &nbsp; Taco Bell', '&nbsp b: &nbsp; Carrabbas', '&nbsp c: &nbsp; A Steakhouse', "&nbsp d: &nbsp; Chili's"],
+            answer: "&nbsp d: &nbsp; Chili's"
 
         },
 
         {
 
             question: 'Every episode of Seinfeld contains an image or reference to what superhero ?',
-            choices: ['Plastic Man', 'Superman ', 'Spiderman', 'Batman'],
-            answer: 'Superman'
+            choices: ['&nbsp a: &nbsp; Plastic Man', '&nbsp b: &nbsp; Superman ', '&nbsp c: &nbsp; Spiderman', '&nbsp d: &nbsp; Batman'],
+            answer: '&nbsp b: &nbsp; Superman'
 
         },
 
         {
 
             question: 'Which pop star played 27 different instruments on their debut album For You?',
-            choices: ['Michael Jackson', 'Guns n Roses', 'Prince', 'Boyz 2 Men'],
-            answer: 'Prince'
+            choices: ['&nbsp a: &nbsp; Michael Jackson', '&nbsp b: &nbsp; Guns n Roses', '&nbsp c: &nbsp; Prince', '&nbsp d: &nbsp; Boyz 2 Men'],
+            answer: '&nbsp c: &nbsp; Prince'
 
         },
     
         {
 
             question: "What was the name of Norm's wife on Cheers?",
-            choices: ['Vera', 'Denise', 'Lucy', 'Sally'],
-            answer: 'Vera'
+            choices: ['&nbsp a: &nbsp; Vera', '&nbsp b: &nbsp; Denise', '&nbsp c: &nbsp; Lucy', '&nbsp d: &nbsp; Sally'],
+            answer: '&nbsp a: &nbsp; Vera'
 
         },
 
         {
 
             question: 'What was the first sport televised in the U.S.?',
-            choices: ['Ping Pong', 'Horse Racing', 'Baseball', 'Football'],
-            answer: 'Baseball'
+            choices: ['&nbsp a: &nbsp; Ping Pong', '&nbsp b: &nbsp; Horse Racing', '&nbsp c: &nbsp; Baseball', '&nbsp d: &nbsp; Football'],
+            answer: '&nbsp c: &nbsp; Baseball'
 
         },
 
         {
 
             question: 'What was the first movie by Pixar to receive a rating higher than G in the United States?',
-            choices: ['Finding Nemo', 'The Incredibles', 'Toy Story 4', 'Monsters Inc'],
-            answer: 'The Incredibles'
+            choices: ['&nbsp a: &nbsp; Finding Nemo', '&nbsp b: &nbsp; The Incredibles', '&nbsp c: &nbsp; Toy Story 4', '&nbsp d: &nbsp; Monsters Inc'],
+            answer: '&nbsp b: &nbsp; The Incredibles'
 
         },
 
         {
 
             question: 'What color are aircraft black boxes? ',
-            choices: ['Bright orange ', 'Black', 'Pink ', 'Transparent '],
-            answer: 'Bright orange '
+            choices: ['&nbsp a: &nbsp; Bright orange ', '&nbsp b: &nbsp; Black', '&nbsp c: &nbsp; Pink ', '&nbsp d: &nbsp; Transparent '],
+            answer: '&nbsp a: &nbsp; Bright orange '
 
         },
 
         {
 
             question: 'What famous general was once attacked by rabbits ?',
-            choices: ['Alexander The Great', 'Julius Caesar', 'Genghis Khan', 'Napolean '],
-            answer: 'Napolean '
+            choices: ['&nbsp a: &nbsp; Alexander The Great', '&nbsp b: &nbsp; Julius Caesar', '&nbsp c: &nbsp; Genghis Khan', '&nbsp d: &nbsp; Napolean '],
+            answer: '&nbsp d: &nbsp; Napolean '
 
         },
 
         {
 
             question: 'Which NFL team appeared in four consecutive Super Bowls from 1991 - 1994 and lost them all? ',
-            choices: ['Buffalo Bills ', 'Washington Redskins', 'Oakland Raiders', 'Chicago Cubs'],
-            answer: 'Buffalo Bills '
+            choices: ['&nbsp a: &nbsp; Buffalo Bills ', '&nbsp b: &nbsp; Washington Redskins', '&nbsp c: &nbsp; Oakland Raiders', '&nbsp d: &nbsp; Chicago Cubs'],
+            answer: '&nbsp a: &nbsp; Buffalo Bills '
 
         },
 
         {
 
             question: 'What is the name of the hobbit played by Elijah Wood in the Lord of the Rings movies?',
-            choices: ['Luke Skywalker ', 'Aragorn ', 'Boromir', 'Frodo '],
-            answer: 'Frodo '
+            choices: ['&nbsp a: &nbsp; Luke Skywalker ', '&nbsp b: &nbsp; Aragorn ', '&nbsp c: &nbsp; Boromir', '&nbsp d: &nbsp; Frodo '],
+            answer: '&nbsp d: &nbsp; Frodo '
 
         }
     
@@ -99,17 +99,40 @@ $(document).ready(function () {
 
 
 
-    //function to load questions//
+    //function to load questions and choices//
     function questionGenerator () {
 
         var newQuestion = triviaQuestions[questionNumber].question;
-        var userChoices = triviaQuestions[questionNumber].choices;
+        var choices = triviaQuestions[questionNumber].choices;
 
         $('#timer').html('Timer: '+ '<br>' + counter);
-        $('#questions').html(newQuestion);
-        console.log (newQuestion);
+        $('#questions').html(`
+            <h1>${newQuestion}</h1><br>
+            <h3>${choicesGenerator(choices)}</h3> 
+        `);
+    
+        
+
+
+        
 
     }
+
+     //function to generate choices corresponding to questions//
+    function choicesGenerator (choices)  {
+
+        var possibleAnswers ='';
+
+        for(i=0; i < choices.length; i++) {
+            possibleAnswers += `<p class= "choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+        }
+
+        return possibleAnswers;
+        
+    }
+
+
+
      questionGenerator();
 
 
